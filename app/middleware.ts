53b04e5da0c64ae8ee656eb965029f10d5e40c70
nikitastogniy812 <nikitastognii@gmail.com>
2024-08-23 13:18:57 +0300

    Update some shit

--- app/middleware.ts
+++ app/middleware.ts
@@ -11,5 +11,5 @@ export default createMiddleware({
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ["/", "/(de|en)/:path*"],
+  matcher: ["/", `/:locale(${locales.join("|")})/:path*`],
 };
