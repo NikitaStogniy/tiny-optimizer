644704675397a23b55e22615ebde736e7f817a91
nikitastogniy812 <nikitastognii@gmail.com>
2024-08-26 22:16:20 +0300

    update compression

--- app/utils/compressor.ts
+++ app/utils/compressor.ts
@@ -6,13 +6,26 @@ import { imageType } from "./types";
 export const Compressor = async (file: File, type: imageType | null) => {
   const buffer = Buffer.from(await file.arrayBuffer());
   const safeType = type ?? "jpeg";
-  if (type === null) {
-    return buffer;
-  }
   try {
-    const optimizedBuffer = await sharp(buffer)
-      .toFormat(safeType, { quality: 95 })
-      .toBuffer();
+    let optimizedBuffer;
+    switch (safeType) {
+      case "jpeg":
+        optimizedBuffer = await sharp(buffer).jpeg({ quality: 85 }).toBuffer();
+        break;
+      case "png":
+        optimizedBuffer = await sharp(buffer)
+          .png({ compressionLevel: 9 })
+          .toBuffer();
+        break;
+      case "webp":
+        optimizedBuffer = await sharp(buffer).webp({ quality: 80 }).toBuffer();
+        break;
+      default:
+        optimizedBuffer = await sharp(buffer)
+          .toFormat(safeType, { quality: 85 })
+          .toBuffer();
+        break;
+    }
     return optimizedBuffer;
   } catch (error) {
     console.error("Error optimizing image:", error);
